1c068f87876bcc84ece05ea876b24f091a554d06
Remka <remuka@gmail.com>
2017-12-11 21:57:23 +0900

    Single book details page.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -52,8 +52,33 @@ exports.book_list = function(req, res, next) {
 };
 
 // Display detail page for a specific book
-exports.book_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = function(req, res, next) {
+
+  async.parallel({
+    book: function(callback) {
+
+      Book.findById(req.params.id)
+        .populate('author')
+        .populate('genre')
+        .exec(callback);
+    },
+    book_instance: function(callback) {
+
+      BookInstance.find({ 'book': req.params.id })
+        //.populate('book')
+        .exec(callback);
+    },
+  }, function(err, results) {
+    if (err) { return next(err); }
+    //Successful, so render
+    res.render('book_detail', {
+      title: 'Title',
+      book: results.book,
+      book_instances: results.book_instance,
+      nav: 'books'
+    });
+  });
+
 };
 
 // Display book create form on GET
